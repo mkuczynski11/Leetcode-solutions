114c8aeb07588da9e2a3e3a5b21cfc8c67ebfe60
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2021-02-16 14:27:43 +0100

    1-solution

---
+++ 1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in d:
+                return [d[a],i]
+            else:
+                d[nums[i]] = i
+        return []
